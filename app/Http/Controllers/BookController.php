7767f4daf5d4ea3b3d28f7377a0f75e373b22774
Dipu Chandra Dey <dipudey764@gmail.com>
2022-07-01 09:59:57 +0600

    add test case for validation and update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,12 +7,42 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    /**
+     * Store Book in a library
+     *
+     * @param Request $request
+     */
     public function store(Request $request)
     {
         Book::query()
-            ->create([
-                'title'  => $request->title,
-                'author' => $request->author
-            ]);
+            ->create($this->validateRequest($request));
+    }
+
+    /**
+     * Book updated
+     *
+     * @param Request $request
+     * @param $id
+     */
+    public function update(Request $request, $id)
+    {
+        Book::query()
+            ->find($id)
+            ->update($this->validateRequest($request));
+    }
+
+    /**
+     * Request Validation
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    private function validateRequest(Request $request)
+    {
+        return $request->validate([
+            'title'  => 'required',
+            'author' => 'required'
+        ]);
     }
 }
--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -28,4 +28,46 @@ class BookReservationTest extends TestCase
 
         $this->assertCount(1, Book::all());
     }
+
+    public function test_title_is_required()
+    {
+        $this->postJson('/api/book', [
+            "title"  => "",
+            "author" => "Test"
+        ])
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
+
+    public function test_author_is_required()
+    {
+        $this->postJson('/api/book', [
+            "title"  => "Code With Fun",
+            "author" => ""
+        ])
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('author');
+    }
+
+    public function test_book_can_be_updated_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->postJson('/api/book', [
+            "title"  => "Code With Fun",
+            "author" => "Test"
+        ])
+            ->assertOk();
+
+        $book = Book::query()->first();
+
+        $this->patchJson("/api/book/{$book->id}", [
+            "title"  => "Coding With Fun",
+            "author" => "Test"
+        ])
+            ->assertOk();
+
+        $this->assertEquals("Coding With Fun", Book::query()->first()->title);
+        $this->assertEquals("Test", Book::query()->first()->author);
+    }
 }
